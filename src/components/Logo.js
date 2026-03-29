af5599a0c5b3529292d1584ac4dc372b3485d77b
parthvadodariya-616 <parthvadodariya2005@gmail.com>
2026-03-30 02:27:17 +0530

    feat: simplify style prop in Logo component for cleaner code

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,12 +4,8 @@ export default function Logo({ size = 36, style = {}, className = '' }) {
   return (
     <img
       src="/assets/images/OmniSensus_logo.svg"
-      style={{
-        height: size * 1.5,
-        width: 'auto',
-        display: 'inline-block',
-        objectFit: 'contain',
-      }}
+      style={{ height: size * 1.5, width: 'auto', display: 'inline-block', objectFit: 'contain', ...style, }}
+      className={className}
     />
   );
 }
